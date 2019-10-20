a0c71d5106650465527346b6c25b4b4d2727b18f
Ghaith Tish <ghaith-tish@hotmail.com>
2019-10-20 09:39:39 +0200

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GraphicsInterface01
+{
+    public partial class Form1 : Form
+    {
+       
+        Shape sh = new Shape();
+        int timer = 0;
+        int Sleep = 1000;
+        public int NumWiederholungen;
+        
+        //public static Double Counter; 
+
+        public Form1()
+        {
+            sh.Counter = 0;
+            
+            //NumWiederholungen = (int)this.Anz_comboBox3.SelectedValue;            
+            InitializeComponent();
+            sh.panel1_Screen = panel1_Screen;
+            sh.panel1_Screen.BackColor = Color.Black;
+
+            comboBox1.Text = "Line";
+           
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            sh.Counter = 0;
+            timer1.Start();
+
+            try
+            {
+                NumWiederholungen = Convert.ToInt32(this.Anz_comboBox3.SelectedItem.ToString());
+            }
+            catch(System.NullReferenceException)
+            {
+                timer1.Stop();
+                MessageBox.Show("Bitte geben Sie die Wiederholungs Nummer an !");
+                
+            }
+
+          
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            timer++;
+
+            int raster = 10;
+            Pen pinsel = new Pen(Color.White, 3);
+            
+            System.Windows.Forms.Timer thisTimer = sender as System.Windows.Forms.Timer;
+
+
+            if (sh.dir.maxsmall == true)
+            {
+                sh.newX -= raster;
+                sh.newY -= raster;
+            }
+            else
+            {
+                sh.newX += raster;
+                sh.newY += raster;
+            }
+
+            
+
+            if (comboBox1.SelectedItem.ToString() == "Rectangle")
+            {
+                sh.drawRect(new Point(sh.newX, sh.newY));
+                //drawRect(new Point(newX, newY));
+                System.Threading.Thread.Sleep(Sleep / trackBar1.Value);
+                sh.Zeichenfläche.Clear(Color.Black);
+            }
+
+            if(comboBox1.SelectedItem.ToString()== "Circle")
+            {
+                sh.Circle(new Point(sh.newX, sh.newY));
+
+                System.Threading.Thread.Sleep(Sleep / trackBar1.Value);
+                sh.Zeichenfläche.Clear(Color.Black);
+            }
+
+            if(comboBox1.SelectedItem.ToString() == "Line")
+            {
+                sh.Line(new Point(sh.newX, sh.newY));
+
+                System.Threading.Thread.Sleep(Sleep / trackBar1.Value);
+                sh.Zeichenfläche.Clear(Color.Black);
+            }
+
+            
+
+            textBox1.Text = sh.Counter.ToString();
+
+
+
+            if (timer % (panel1_Screen.Width / 10) == 0)
+            {
+                sh.Counter += 1;
+
+                if (sh.Counter >= NumWiederholungen)
+                {
+                    timer1.Stop();
+                }
+            }
+        }
+    }
+}
---
+++ Shape.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GraphicsInterface01
+{
+    class Shape
+    {
+        public int newX;
+        public int newY;
+        public PictureBox panel1_Screen;
+        public int Counter;
+
+
+
+        public Graphics Zeichenfläche;
+        public Richtung dir = new Richtung();
+
+
+        public struct Richtung
+        {
+            public bool maxsmall;
+            public bool maxLarge;
+        }
+
+        public void drawRect(Point po)
+        {
+
+            Zeichenfläche = panel1_Screen.CreateGraphics();
+            int rectWidth = panel1_Screen.Width - (po.X * 2);
+            int rectHeight = panel1_Screen.Height - (po.Y * 2);
+
+            Pen pinsel = new Pen(Color.White, 5);
+            Rectangle rect = new Rectangle(po.X, po.Y, rectWidth, rectHeight);
+            Zeichenfläche.DrawRectangle(pinsel, rect);
+
+            if (rect.Width <= panel1_Screen.Width / 5)
+            {
+                
+                dir.maxsmall = true;
+                dir.maxLarge = false;
+                
+                
+            }
+            if (rect.Width >= panel1_Screen.Width)
+            {
+                dir.maxLarge = true;
+                dir.maxsmall = false;
+            }
+
+            return ;
+        }
+
+        public void  Circle(Point po)
+        {
+            Zeichenfläche = panel1_Screen.CreateGraphics();
+            int rectWidth = panel1_Screen.Width - (po.X * 2);
+            int rectHeight = panel1_Screen.Height - (po.Y * 2);
+
+            Pen pinsel = new Pen(Color.White, 5);
+            Rectangle rect = new Rectangle(po.X, po.Y, rectWidth, rectHeight);
+
+            Zeichenfläche.DrawEllipse(pinsel, rect);
+            
+
+            if (rect.Width <= panel1_Screen.Width / 5)
+            {
+
+                dir.maxsmall = true;
+                dir.maxLarge = false;
+
+
+            }
+            if (rect.Width >= panel1_Screen.Width)
+            {
+                dir.maxLarge = true;
+                dir.maxsmall = false;
+            }
+
+            return;
+        }
+
+        public void Line(Point po)
+        {
+            Zeichenfläche = panel1_Screen.CreateGraphics();
+            
+            Pen pinsel = new Pen(Color.White, 5);
+            
+            Point StPt = new Point(0, po.Y + panel1_Screen.Height/2);
+            Point EndPt = new Point(panel1_Screen.Width, po.Y + panel1_Screen.Height / 2);
+
+            Point StPt1 = new Point(0, -po.Y + panel1_Screen.Height / 2);
+            Point EndPt1 = new Point(panel1_Screen.Width, - po.Y + panel1_Screen.Height / 2);
+
+            Zeichenfläche.DrawLine(pinsel, StPt, EndPt);
+            Zeichenfläche.DrawLine(pinsel, StPt1, EndPt1);
+
+            if (po.Y + panel1_Screen.Height / 2 >= panel1_Screen.Height)
+            {
+
+                dir.maxsmall = true;
+                dir.maxLarge = false;
+
+
+            }
+            if (po.Y + panel1_Screen.Height / 2 <= panel1_Screen.Height/2)
+            {
+                dir.maxLarge = true;
+                dir.maxsmall = false;
+            }
+
+            return;
+
+
+        }
+
+    }
+}
